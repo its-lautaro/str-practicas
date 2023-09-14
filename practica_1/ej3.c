f9bec1399bf69b77646ff18197f0bff69467245b
Gabriel Torres <111906750+gabitorres2@users.noreply.github.com>
2023-09-14 13:59:41 -0300

    Add files via upload

--- practica_1/ej3.c
+++ practica_1/ej3.c
@@ -1,27 +1,35 @@
 #include <htc.h>
 
-
 void initADC(){
-	ADCON1 = 0b10001110; //justificacion a derecha y AN0 como entrada analogica 
-	ADCON0 = 0b01000001; //osc/8, ADON activado
+	ADCON1 = 0b10001110; //justificacion a derecha y RA0 como entrada analogica 
+	ADCON0 = 0b01000001; //Fosc/8, ADON activado
 	ADIE = 0; 
+}
+
+void initMCU(){
+	RD6 = 0;
+	RD7 = 0;
 	TRISA |= 1; //setear AN0/RA0 como entrada
+	TRISB = 0; //PORTB como salida
+	TRISD = 0b00111111; //RB6 y RB7 como salida
 }
 
-unsigned short poll(){
+void polling(){
 	ADCON0 |= (1<<2); //activa la conversion 
-	while((ADCON0 &(1<<2))){} 
-	return (ADRESH << 8 | ADRESL) ;
+	while((ADCON0 &(1<<2)));
+	PORTB = ADRESH; //envia la parte alta
+	RD7 = 1; //activa LE del latch U3
+	RD7 = 0;
+	PORTB = ADRESL; //envia la parte baja 
+	RD6 = 1; //activa LE del latch U2
+	RD6 = 0; 
 }
 
-void sendValue(v){
-	PORTB = 
-}
 
 int main(){
-	unsigned short valor;
+	initMCU();
 	initADC(); //configuracion inicial del ADC
 	while(1){
-		valor = poll();
+		polling();
 	} 
 }
